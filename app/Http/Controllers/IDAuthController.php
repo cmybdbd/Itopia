82e5b4e68534a945684eb2c0141c0542fa4e5bac
wjllance <wjllance@wjllancedeMacBook-Air.local>
2017-04-13 23:38:23 +0800

    test

--- app/Http/Controllers/IDAuthController.php
+++ app/Http/Controllers/IDAuthController.php
@@ -68,6 +68,6 @@ class IDAuthController extends Controller
 
     function getMillisecond() {
         list($t1, $t2) = explode(' ', microtime());
-        return (float)sprintf('%.0f',(floatval($t1)+floatval($t2))*1000);
+        return (string)sprintf('%.0f',(floatval($t1)+floatval($t2))*1000);
     }
 }
