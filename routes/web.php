64cfca37f926883605b029241e5219996689bc11
smallst <tongsmallst@gmail.com>
2017-04-15 22:46:53 +0800

    test multi prefix

--- routes/web.php
+++ routes/web.php
@@ -56,25 +56,18 @@ Route::group(['prefix' => 'lock'], function(){
     Route::get('updatePassword', "LockController@api_update_password");
 });
 
-Route::group(['prefix' => 'itopia'], function(){
-Route::get('/oauth_callback', 'WeChatController@call_back');
-Route::get('/payment/callback', 'WeChatController@payment_call_back');
-Route::get('/server', 'WeChatController@check_server');
+Route::group(['prefix' => 'itopia'], function()
+{
+    Route::get('/oauth_callback', 'WeChatController@call_back');
+    Route::get('/payment/callback', 'WeChatController@payment_call_back');
+    Route::get('/server', 'WeChatController@check_server');
 
 
-Route::post('/order/create', 'OrderController@storeOrder');
-Route::post('/comment/create', 'CommentController@store');
-Route::get('/test', function (){
-    echo (time() % (24*60*60) > 12 * 60*60 )? 't':'f';
-    echo (time() % (24*60*60));
-    return ;
-});
-Route::get('test2', function (){
-    echo (time() % (24*60*60));
-    return ;
+    Route::post('/order/create', 'OrderController@storeOrder');
+    Route::post('/comment/create', 'CommentController@store');
 });
 
-Route::group(['middleware' => 'auth'], function (){
+Route::group(['middleware' => 'auth','prefix'=>'itopia'], function (){
     Route::get('/home', 'HomeController@index')->name('home');
     Route::get('/create/{uid}/{rid}', 'OrderController@createOrder');
     Route::get('/result/{id}','OrderController@getOrderDetail');
@@ -85,17 +78,19 @@ Route::group(['middleware' => 'auth'], function (){
     Route::get('/manage/room', 'RoomController@manageRoom');
     Route::get('/manage/order', 'OrderController@manageOrder');
 });
-Route::group(['middleware' => ['web','wechat.oauth']], function () {
+Route::group(['middleware' => ['web','wechat.oauth'],'prefix'=>'itopia'], function () {
     Route::get('/login', 'WeChatController@auth')->name('login');
 });
 
 
+Route::group(['prefix' => 'itopia'], function()
+{
 Route::get('/lock/callback', 'LockController@callback');
 
 Route::get('/sendCode', 'SMSController@sendCode');
 Route::get('/idAuth', 'IDAuthController@IDauth');
-Route::group(['prefix' => 'lock'], function(){
+});
+Route::group(['prefix' => 'itopia/lock'], function(){
     Route::get('updatePassword', "LockController@api_update_password");
 });
 
-});
