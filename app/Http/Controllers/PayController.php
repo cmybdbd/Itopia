4e6eb7e78b2a85897e6ecbdb9c80344dfea25068
wjllance <wjllance@wjllancedeMacBook-Air.local>
2017-04-16 09:10:10 +0800

    pay controller generate order

--- app/Http/Controllers/PayController.php
+++ app/Http/Controllers/PayController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\ApiHandle;
+use App\Order;
+use App\Utils\Constant;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Response;
 
 class PayController extends Controller
 {
@@ -17,23 +21,53 @@ class PayController extends Controller
 
     public function generateOrder($tenantOrder, $money, $userId, $userName, $idcard, $productInfo, $phone)
     {
-        $sign = getsig();
-
-        $money_aes = doAES($money);
-
+        $query_url = $this->api_url;
         $params = array(
             "tenantId" => $this->tenanId,
             "tenantOrder" => $tenantOrder,
             'signType' => 'MD5',
-            'frontUrl' => $this->front_url,
-            'backUrl' => $this->back_url,
+            'money' => doAES($money),
+            'frontUrl' => $this->doAES($this->front_url),
+            'backUrl' => $this->doAES($this->back_url),
             'customerId' => $userId,
             'userNmae' => $userName,
             'idcard' => $idcard,
             'productInfo' => $productInfo,
             'phone' => $phone,
-            'money' => $money_aes
         );
+        $sign = getSignature($params);
+        $params['sign'] = $sign;
+
+        $res = ApiHandle::httpPostJson($query_url, $params);
+        $res = json_decode($res, true);
+        var_dump($res);
+        if($res['resultCode'] == '000')
+        {
+            $orderno = $res['orderno'];
+            $order = Order::find($tenantOrder);
+            $order->orderno = $orderno;
+            $order->save();
+            //...
+
+            $ret = array(
+                'code' => 200,
+                'content' => array(
+                    'payUrl' => $res['payUrl']
+                )
+            );
+        }
+        else
+        {
+            $ret = array(
+                'code' => Constant::$STATUS_CODE['GENERATE_ORDER_FAIL'],
+                'content' => $res
+            );
+        }
+        return Response::json($ret);
     }
 
+    private function doAES($str)
+    {
+
+    }
 }
