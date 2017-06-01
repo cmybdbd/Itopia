3ee83022bef2d20472bd893a3eb79fa5de880f34
MarcWong <brucewayne@pku.edu.cn>
2017-06-01 10:52:09 +0800

    map fix

--- resources/views/map/mapIndex.blade.php
+++ resources/views/map/mapIndex.blade.php
@@ -24,10 +24,10 @@
         </div>
     </div>
     <div style="position:fixed;width:100%;bottom:5%;">
-        <div class="circle" style="text-align:center;position:absolute;bottom:0%;left:55px">
+        <div class="circle" style="text-align:center;position:absolute;bottom:0;left:20%;">
             <p class="font-xl" style="margin-top:16px;font-weight:500;">时租</p>
             <p class="font-l m-color" style="margin-top:-10px;font-weight:500;">19 / 小时</p></div>
-        <div class="circle" style="text-align:center;position:absolute;bottom:0%;right:55px">
+        <div class="circle" style="text-align:center;position:absolute;bottom:0;right:20%;">
             <p class="font-xl" style="margin-top:16px;font-weight:500;">包夜</p>
             <p class="font-l m-color" style="margin-top:-10px;font-weight:500;">159 / 夜</p></div>
     </div>
