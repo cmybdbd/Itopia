2e667132e7ef6a2511f8b7c81ad61bbc7f13aebc
MarcWong <brucewayne@pku.edu.cn>
2017-06-01 11:13:21 +0800

    font-family added

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -20,6 +20,7 @@
         /*@font-face { font-family:Noto-Sans; src: url('font/NotoSans-Bold.ttf'); }*/
 
         body{
+            font-family: NexaLight,-apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"; 
             background-color: white;
         }
         .lr{
