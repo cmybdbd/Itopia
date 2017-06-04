e030d1784c646424b7185ac567e65e503ea5e25a
MarcWong <brucewayne@pku.edu.cn>
2017-06-04 16:09:52 +0800

    login sb fix

--- public/js/login.js
+++ public/js/login.js
@@ -56,45 +56,7 @@ $(function () {
     $(window).bind("resize", ScaleSlider);
     $(window).bind("orientationchange", ScaleSlider);
 */
-
-
-    
-    $("#equipment").on('click',function () {
-        $(".equipment-content").modal();
-    });
-
-    var validatePhone =$("#validatePhone");
-    var phoneN = $("#phoneN");
-    validatePhone.on('shown.bs.modal', function () {
-        phoneN.focus();
-    });
-
-
-    var uid = $("#param .uid").attr('data-content');
-    $("#myOrder").on('click',function () {
-        if(!$("#param .uphoneN").attr("data-content"))
-            login();
-        else
-            window.location.href = window.location.href.replace('home','orderList/'+uid);
-    });
-    $("#day").on('click',function () {
-        if(!$("#param .uphoneN").attr("data-content"))
-            login();
-        else
-        window.location.href = window.location.href.replace('home','dayPage');
-    });
-    $("#night").on('click',function () {
-        if(!$("#param .uphoneN").attr("data-content"))
-            login();
-        else
-        window.location.href = window.location.href.replace('home','nightPage');
-    });
-    
-    //$(document).ready(login());
-
-});
-
-function login () {
+    function login () {
         var t1 =false,t2=false;
         
         if(!$("#param .uphoneN").attr("data-content")) {
@@ -270,3 +232,39 @@ function login () {
             window.location.href = '/home';
         }
     }
+
+    
+    $("#equipment").on('click',function () {
+        $(".equipment-content").modal();
+    });
+
+    var validatePhone =$("#validatePhone");
+    var phoneN = $("#phoneN");
+    validatePhone.on('shown.bs.modal', function () {
+        phoneN.focus();
+    });
+
+
+    var uid = $("#param .uid").attr('data-content');
+    $("#myOrder").on('click',function () {
+        if(!$("#param .uphoneN").attr("data-content"))
+            login();
+        else
+            window.location.href = window.location.href.replace('home','orderList/'+uid);
+    });
+    $("#day").on('click',function () {
+        if(!$("#param .uphoneN").attr("data-content"))
+            login();
+        else
+        window.location.href = window.location.href.replace('home','dayPage');
+    });
+    $("#night").on('click',function () {
+        if(!$("#param .uphoneN").attr("data-content"))
+            login();
+        else
+        window.location.href = window.location.href.replace('home','nightPage');
+    });
+    
+    $(document).ready(login());
+
+});
