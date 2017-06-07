60b80ab3bd3b37d86549f0504520b18d443ba761
MarcWong <brucewayne@pku.edu.cn>
2017-06-07 14:44:52 +0800

    create day order

--- resources/views/order/create.blade.php
+++ resources/views/order/create.blade.php
@@ -154,14 +154,14 @@
                 <!--<span>{{$room->nextDayUsingTime()}}</span>
                 <br>
                 <span>nextTime= {{date('d-H:i',$room->nextTime())}}</span>
-                <br>
-                <span>startDayTime={{date('d-H:i',$startDayTime)}}</span>-->
+                <br>-->
+                <span>startDayTime={{date('d-H:i',$startDayTime)}}</span>
                 <?php
                 $remainTime = (22.5 - date("H") - $room->type/2.0) + (-date("i"))/60; 
                 $remainOrderTime = (22.5 - date('H',$room->nextTime()) - $room->type/2.0) + (-date('H=i',$room->nextTime()))/60;
                 
-                if(($startDayTime- 57600) %86400 == 0)
-                    $startDayTime = $startDayTime + (11 - $room->type/2.0) * 3600;
+                if($startDayTime %86400 == 0)
+                    $startDayTime = strtotime(date("Y-n-d")) + (11 - $room->type/2.0) * 3600 + 86400;
 
                 if(date('H',$room->nextTime()) != 8 && $remainOrderTime > 1)
                 {
