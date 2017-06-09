49eb51a7b470bf78824c1c52d0eeca6abc122b72
MarcWong <brucewayne@pku.edu.cn>
2017-06-09 18:12:22 +0800

    list page nav

--- resources/views/order/list.blade.php
+++ resources/views/order/list.blade.php
@@ -20,19 +20,20 @@
                 @if($key != 0)
                     <hr class="mysplit">
                 @endif
-            <div class="order-item" data-content="{{$order->id}}">
+            <div class="order-item" id="{{$order->id}}" data-content="{{$order->state}}">
                 <div class="f-color">
                     <span>{{date('m月d日',strtotime($order->startTime))}}</span>
                     -
                     <span>{{$order->isDay?'分时使用':'包夜使用'}}</span>
                     @if($order->state < 4)
-                        <span id="{{$order->id}}" class="button-occupied font-s" style="width:55px;height:24px;float:right;">已结束</span>
+                        <span class="button-occupied font-s" style="width:55px;height:24px;float:right;">已结束</span>
                     @elseif($order->state ==10)
-                        <span id="{{$order->id}}" class="button-available font-s" style="width:55px;height:24px;float:right;">待评价</span>
-                    @elseif($order->state <6)
-                        <span id="{{$order->id}}" class="button-available font-s" style="width:55px;height:24px;float:right;">可使用</span>
+                        <span class="button-available font-s" style="width:55px;height:24px;float:right;">待评价</span>
+                    @elseif($order->state <7)
+                        <span class="button-available font-s" style="width:55px;height:24px;float:right;">使用中</span>
                     @else
-                        <span id="{{$order->id}}" class="button-available font-s" style="width:55px;height:24px;float:right;">使用中</span>
+                        <span class="button-available font-s" style="width:55px;height:24px;float:right;">可使用</span>
+                    
                     @endif
                 </div>
                 <div class="b-color">
@@ -64,8 +65,22 @@
     <script>
         $(function () {
             $(".order-item").on('click',function(){
-                //switch
-                window.location.href = '/result/'+$(this).attr("data-content");
+                state = $(this).attr('data-content');
+                switch(state)
+                {
+                    case '5':
+                        window.location.href = '/result/'+$(this).attr("id");
+                        break;
+                    case '6':
+                        window.location.href = '/result/'+$(this).attr("id");
+                        break;
+                    case '10':
+                        window.location.href = '/comment/'+$(this).attr("id");
+                        break;
+                    default:
+                        break;
+                }
+                
             });
         })
     </script>
