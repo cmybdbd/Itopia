5f5179d1975713c0cfa7c70b3fecd23a1cb71af3
MarcWong <brucewayne@pku.edu.cn>
2017-06-09 17:14:59 +0800

    fix

--- resources/views/list/dayRoom.blade.php
+++ resources/views/list/dayRoom.blade.php
@@ -104,15 +104,18 @@
                             $remainTime = (22.5 - date("H") - $room->type/2.0) + (-date("i"))/60; 
                             $s = $room->nextTime() - $room->type/2.0 * 3600; 
                             $remainOrderTime = (22.5 - date('H',$room->nextTime()) - $room->type/2.0) + (-date('H:i',$room->nextTime()))/60;
+
                             if($room->nextDayUsingTime() == 0)
                                 $tomorrowTime = $room->type==1 ? '11:30':'12:00';
                             else if ($room->nextDayUsingTime() == -1)
                                 $tomorrowTime = -1;
                             else
                                 $tomorrowTime = date("H:i",$room->nextDayUsingTime());
+
+                            if($room->nextTime()==0)
+                                $remainOrderTime=0;
                         ?>
                         
-                        <!--{{$remainOrderTime}}-->
                     
                         
                         @if(date('H',$room->nextTime()) != 8 && $remainOrderTime > 0.5)
@@ -146,7 +149,7 @@
                                 @endif
                             @endif
                         @elseif(date('H',$room->nextTime()) == 8)
-                            @if($remainTime>1)
+                            @if($remainOrderTime>0.5)
                                 <span id="btn{{$room->state}}" data-content="1" class="room-state b-color font-s" style="float:right;">即时使用</span>
                                 <!--state 5-->
                             @else
@@ -157,6 +160,7 @@
                                 <span id="btn{{$room->state}}" data-content="0" class="room-state b-color font-s" style="float:right;">今日已约满</span>
                                 <!--state 7-->
                         @endif
+                        <div id="nexttime{{$room->state}}" data-content = "{{$room->nextTime()}}"></div>
                         <div id="nextdaytime{{$room->state}}" data-content = "{{$tomorrowTime}}"></div>
                         <div id="remaintime{{$room->state}}" data-content = "{{$remainTime}}"></div>
                          <div id="remainordertime{{$room->state}}" data-content = "{{$remainOrderTime}}"></div>
