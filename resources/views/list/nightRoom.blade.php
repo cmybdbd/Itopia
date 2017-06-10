b69a54fbf618a3978bfdec966e5189fecf1f7870
MarcWong <brucewayne@pku.edu.cn>
2017-06-10 17:15:32 +0800

    test

--- resources/views/list/nightRoom.blade.php
+++ resources/views/list/nightRoom.blade.php
@@ -145,7 +145,6 @@
 @section('scripts')
     <script src="{{url('js/jssor.slider.min.js')}}"></script>
     <script src="{{url('js/roomList.js')}}"></script>
-    <!--script src="{{url('js/login.js')}}"></script>-->
     <script>
     alert('今日(6月10日)17:30-20:00为内测时间，请非测试人员不要下单，否则后果自负！ps:先前已下订单不受影响');
     </script>
