cbafc96e6880a9b93e2354b16999048da0e6840c
leonardo <806218468@qq.com>
2017-06-13 21:45:55 +0800

    fakeOrder background fixed

--- resources/views/manage/room.blade.php
+++ resources/views/manage/room.blade.php
@@ -483,7 +483,8 @@ function showHumanDay(ts)
                     'startTime': st,
                     'endTime'  : ed,
                     'duration' : $('#selectTime').attr('data-content'),//+durationTime.attr('data-content')/3600000,
-                    'isDay'    : 1
+                    'isDay'    : 1,
+                    'day'      : 0
                     };
                 console.log(data);
                 $.ajax({
@@ -496,8 +497,7 @@ function showHumanDay(ts)
                         //alert(' ');
                         if(param['code'] == '200')
                         {
-                            alert('成功占用');
-                            console.log(param);
+                            alert(param['param']);
                         }
                         else
                         {
@@ -512,13 +512,14 @@ function showHumanDay(ts)
             });
 
             $('#nightUse').on('click',function(){
-                alert('click nightuse');
+                //alert('click nightuse');
                 data = {
                     'roomId': $(this).attr('data-content'),
                     'startTime': $('#endNightTime').attr('data-content')*1.0+ 86400 * $('#selectNight').attr('data-content'),
                     'endTime'  : $('#startNightTime').attr('data-content')*1.0 + 86400 * $('#selectNight').attr('data-content'),
                     'duration' : 12,//+durationTime.attr('data-content')/3600000,
-                    'isDay'    : 0
+                    'isDay'    : 0,
+                    'day'      : $('#selectNight').attr('data-content')
                     };
                 console.log(data);
                 $.ajax({
@@ -531,8 +532,8 @@ function showHumanDay(ts)
                         //alert(' ');
                         if(param['code'] == '200')
                         {
-                            alert('成功占用');
-                            console.log(param);
+                            ///alert('成功占用');
+                            alert(param['param']);
                         }
                         else
                         {
