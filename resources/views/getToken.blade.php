e46a25a0bfcc4e8df96ed4e534d5ca7fb6d2a359
MarcWong <brucewayne@pku.edu.cn>
2017-06-14 14:30:44 +0800

    rollback

--- resources/views/getToken.blade.php
+++ resources/views/getToken.blade.php
@@ -12,7 +12,6 @@
         $.ajax({
             url:'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxa6e10a805f012943&secret=8eab99771e7587f0ee615476964cf5c6',
             type: 'GET',
-            async : false,
             success: function(reg){
                 $('#response').text('success');
             },
