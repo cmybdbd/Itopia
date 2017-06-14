7c505ecbc9ec8f4e2ebbb90d3a2b629897d7f5df
MarcWong <brucewayne@pku.edu.cn>
2017-06-14 12:24:06 +0800

    grantType added

--- resources/views/map/mapWX.blade.php
+++ resources/views/map/mapWX.blade.php
@@ -5,8 +5,8 @@
     wx.config(<?php echo $js->config(array('onMenuShareQQ', 'onMenuShareWeibo','openLocation','getLocation'), true) ?>);
     document.querySelector('#openLocation').onclick = function () {
     wx.openLocation({
-      latitude: 23.099994,
-      longitude: 113.324520,
+      latitude: 38.099994,
+      longitude: 115.324520,
       name: 'hi',
       address: '123',
       scale: 14,
@@ -14,7 +14,6 @@
     });
   };
 
-  // 7.2 鑾峰彇褰撳墠鍦扮悊浣嶇疆
   document.querySelector('#getLocation').onclick = function () {
     wx.getLocation({
       success: function (res) {
@@ -28,9 +27,9 @@
 </script>
 @endsection
 @section('content')
-  <h3 id="menu-location">地理位置接口</h3>
-      <span class="desc">使用微信内置地图查看位置接口</span>
+  <h3>地理位置接口</h3>
+      <span>使用微信内置地图查看位置接口</span>
       <button class="btn btn_primary" id="openLocation">openLocation</button>
-      <span class="desc">获取地理位置接口</span>
+      <span>获取地理位置接口</span>
       <button class="btn btn_primary" id="getLocation">getLocation</button>
 @endsection
